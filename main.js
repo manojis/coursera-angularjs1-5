6b3e75330aec066cbd5b417b249e800e534fe75e
manoj_m <manojis@live.com>
2016-09-20 13:36:48 -0500

    First angularjs file

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
 (function () {
-  'use strict';
+'use strict';
 
-angular.module('NameCalculator',[])
-.controller('NameCalculatorController',['$scope','$filter',diController]);
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+.filter('loves',lovesFilter);
 
-function diController($scope,$filter){
-    $scope.name="manoj";
+MsgController.$inject = ['$scope','lovesFilter'];
+function MsgController($scope,lovesFilter) {
+  $scope.name = "Yaakov";
+  $scope.stateOfBeing = "hungry";
 
-    $scope.displayNumeric = function(){
-      var upper = $filter('uppercase');
-      $scope.name = upper($scope.name);
-    };
+  $scope.sayMessage = function () {
+    return "Yaakov likes to eat healthy snacks at night!";
+  };
+
+  $scope.sayLovesMessage = function () {
+    var msg = "Yaakov likes to eat healthy snacks at night!";
+    msg = lovesFilter(msg);
+    return msg;
+  };
+
+  $scope.feedYaakov = function () {
+    $scope.stateOfBeing = "fed";
+  };
 }
+
+function lovesFilter($scope) {
+  return function(input){
+    input = input || "";
+    iput = input.replace("likes","loves");
+    return input;
+  }
+}
+
 })();
